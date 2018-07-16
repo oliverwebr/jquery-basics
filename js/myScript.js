c338c1f5635dc591543d97154a5bfba29f1fce49
Oliver Weber <oliver@mondough.io>
2018-07-16 15:44:08 +0200

    Task 5

--- js/myScript.js
+++ js/myScript.js
@@ -64,3 +64,46 @@ $placeHolderImages.each((index, ele) => {
     }
   })
 })
+
+// Task 5
+$("body").append(`
+  <div id="bestModalInThisWorldForSure" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+`)
+
+$portfolioItems.click(function(e) {
+  e.preventDefault()
+  const $modal = $("#bestModalInThisWorldForSure")
+  const $modalTitle = $modal.find(".modal-title")
+  const $modalBody = $modal.find(".modal-body")
+
+  // empty modal
+  $modalTitle.empty()
+  $modalBody.empty()
+
+  // Get values from clicked portfolio Item
+  const $image = $(this)
+    .find("img")
+    .clone()
+  const $title = $(this).find(".card-title a")
+
+  // Fill Modal
+  $modalTitle.text($title[0].text)
+  $modalBody.append($image)
+
+  // Show
+  $modal.modal("show")
+})
